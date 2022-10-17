3d4b96d4f9c5549b1cd55052c75ba4db2126837e
lnoppinger <lukas.noppinger@gmail.com>
2022-10-17 14:29:26 +0000

    modified:   .devcontainer/db/init.sql 	modified:   .devcontainer/devcontainer.json 	modified:   .devcontainer/docker-compose.yml 	modified:   .dockerignore 	modified:   .env.expample 	modified:   .gitignore 	modified:   Dockerfile 	modified:   modules/db.js 	modified:   modules/preis.js 	modified:   package-lock.json 	modified:   package.json 	new file:   public/css/icons.css 	new file:   public/fonts/material-symbols-outlined.woff2 	modified:   routes/api/_routes.js 	modified:   routes/api/berechnen/middleware.js 	modified:   routes/api/berechnen/preis.js 	modified:   routes/api/berechnen/zutaten.js 	modified:   routes/api/rezept.js 	modified:   routes/api/zutat.js 	modified:   routes/error.js 	modified:   routes/render.js 	modified:   routes/routes.js 	modified:   setup/default.sh 	modified:   start.js 	modified:   views/berechnen.ejs 	modified:   views/home.ejs 	modified:   views/rezept-bearbeiten.ejs 	modified:   views/rezept.ejs 	modified:   views/zutat-bearbeiten.ejs 	modified:   views/zutat.ejs

--- modules/db.js
+++ modules/db.js
@@ -7,7 +7,7 @@ let db = pgp({
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    database: "Warenwirtschaft"
+    database: process.env.POSTGRES_DATABASE
 })
 
 
